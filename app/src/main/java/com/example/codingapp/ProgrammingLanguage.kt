d72273eb603d0c8583fbd66f9c017479c1173e12
= <=abhishek.sharma4@mygeorgianc.on.ca>
2019-11-10 19:28:58 -0500

    Adding authentication utilites of google and email

--- app/src/main/java/com/example/codingapp/ProgrammingLanguage.kt
+++ app/src/main/java/com/example/codingapp/ProgrammingLanguage.kt
@@ -3,11 +3,10 @@ package com.example.codingapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ProgrammingLanguage : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_programming_language)
     }
 }
-
